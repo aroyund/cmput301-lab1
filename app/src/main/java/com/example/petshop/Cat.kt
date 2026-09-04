843e58dff4deaef6f2bf83b2d587b49ff2fd3270
Marko Kljakic <marko.kljakic1@gmail.com>
2026-09-03 18:19:49 -0600

    Add CMPUT 301 files

---
+++ app/src/main/java/com/example/petshop/Cat.kt
@@ -0,0 +1,14 @@
+package com.example.petshop
+
+class Cat(name: String, age: Int): Pet(name, age), Pettable {
+    override fun speak(): String {
+        return "Meow"
+    }
+    override fun pet() {
+        println("The cat $name is being petted")
+    }
+
+    override fun currentMood(): String {
+        return "Current mood is $date"
+    }
+}
---
+++ app/src/main/java/com/example/petshop/Scorpion.kt
@@ -0,0 +1,6 @@
+package com.example.petshop
+class Scorpion(name: String, age: Int): Pet(name, age) {
+    override fun speak(): String {
+        return "hiss"
+    }
+}
